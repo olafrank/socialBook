59a5a9905022a85965b51269deeb7b620c6bef07
olafrank <olafrnk@gmail.com>
2023-02-10 12:48:35 +0100

    first commit

---
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -0,0 +1,35 @@
+import { ButtonBase, Typography,useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+
+const AdvertWidget = () =>{
+    const {palette} = useTheme();
+    const dark = palette.neutral.dark;
+    const main = palette.neutral.main;
+    const medium = palette.neutral.medium;
+
+    return(
+        <WidgetWrapper>
+            <FlexBetween>
+                <Typography color={dark} variant="h5" fontWeight="500">
+                    Sponsored
+                </Typography>
+                <Typography color={medium}>Create Ad</Typography>
+            </FlexBetween>
+                <img
+                width="100%"
+                height="auto"
+                alt="advert"
+                src="http://localhost:3001/assets/afah.png" 
+                style={{borderRadius:"0.75rem",margin:"0.75rem 0"}}/>
+            <FlexBetween>
+                <Typography color={main}>Afah collections</Typography>
+                <Typography color={medium}><ButtonBase>https://afah.vercel.app</ButtonBase></Typography>
+            </FlexBetween>
+            <Typography color={medium} m="0.5rem 0">You have you covered for all fascinator design and hat,great quality</Typography>
+        </WidgetWrapper>
+    )
+
+}
+
+export default AdvertWidget;
